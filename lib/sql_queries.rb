14f1d89cff3c03a944965f6243dbfa261881efbf
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-17 01:02:30 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -26,7 +26,8 @@ def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_
   "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
   FROM projects
   INNER JOIN pledges
-  ON pledges.project_id = projects.id HAVING SUM(pledges.amount) > projects.funding_goal; "
+  ON pledges.project_id = projects.id
+  GROUP BY projects.title HAVING SUM(pledges.amount) > projects.funding_goal; "
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
